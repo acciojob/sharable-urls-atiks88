067f385eaa5e0fc145e441bc2d7d03792f6ee831
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-10-23 19:20:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// your code here
+const form = document.getElementById("myForm");
+const urlElement = document.getElementById("url");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const year = document.getElementById("year").value;
+
+    const queryString = `?name=${name}&year=${year}`;
+    urlElement.textContent = "https://localhost:8080/" + queryString;
+});
+
